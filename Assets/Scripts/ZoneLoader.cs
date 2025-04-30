f9dc90414798580a4d872dc6312635b4c978b18c
Diana <114948133+Dinasi4ka@users.noreply.github.com>
2025-04-30 19:16:41 +0300

    add box collider

---
+++ Assets/Scripts/ZoneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZoneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("Zone1_Penguin", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync("Zone3_Tiger", LoadSceneMode.Additive);
+    }
+}
