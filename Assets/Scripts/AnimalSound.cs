2ef1b2c2ffcc513606057112f43dcaa7cd95b5ab
Diana <114948133+Dinasi4ka@users.noreply.github.com>
2025-05-21 21:05:46 +0300

    structured the project, added a skybox and audio to the tiger zone

---
+++ Assets/Scripts/AnimalSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AnimalSoundZone : MonoBehaviour
+{
+    public AudioSource animalSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (animalSound != null && !animalSound.isPlaying)
+            {
+                animalSound.Play();
+            }
+        }
+    }
+}
